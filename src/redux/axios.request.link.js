d44f893f0a67c5f51c2624f4514e676fdb53aebd
rasheeddavid <griffinc317@gmail.com>
2019-05-07 13:51:50 +0100

    hosted | api

--- src/redux/axios.request.link.js
+++ src/redux/axios.request.link.js
@@ -1,3 +1,3 @@
-export const MOVIE_DASHBOARD_API =
+export const MOVIE_DASHBOARD_API_LOCAL =
     'https://expressjs-starter-kit-yjwapsqcyk.now.sh/api/v1',
-  MOVIE_DASHBOARD_API_LOCAL = 'http://localhost:5000/api/v1';
+  MOVIE_DASHBOARD_API = 'http://localhost:5000/api/v1';
