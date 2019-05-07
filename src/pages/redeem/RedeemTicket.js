16577440616d45d38c98b42d85c87308cde77e1c
Olutunmbi Banto <olutunmbi@nairabox.com>
2019-05-07 15:04:48 +0100

    layout update for redeem ticket

--- src/pages/redeem/RedeemTicket.js
+++ src/pages/redeem/RedeemTicket.js
@@ -13,6 +13,7 @@ import {
   Input,
   Label,
   Row,
+  Table,
 } from 'reactstrap';
 import DatePicker from 'react-datepicker';
 import randomString from 'randomstring';
@@ -32,6 +33,13 @@ class RedeemTicket extends Component {
               <CardHeader>Redeem Ticket</CardHeader>
               <CardBody>
                 <Form>
+                  {/* <Col xl={6} lg={6} md={6}>
+
+                  </Col>
+                  <Col xl={6} lg={6} md={6}>
+
+                  </Col> */}
+
                   <FormGroup row>
                     <Label for="phone" sm={3}>
                       Phone Number
@@ -55,10 +63,51 @@ class RedeemTicket extends Component {
                   </FormGroup>
                   <FormGroup check row>
                     <Col sm={{ size: 9, offset: 3 }}>
-                      <Button>Redeem Ticket</Button>
+                      <Button>Show Ticket Details</Button>
                     </Col>
                   </FormGroup>
                 </Form>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Card>
+              <CardHeader>Ticket Details</CardHeader>
+              <CardBody>
+                <Table striped={true} id="listmerchantstable">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Customer Details</th>
+                      <th>Movie Details</th>
+                      <th>Purchase Details</th>
+                      <th>Validity</th>
+                      <th>Amount</th>
+                      <th>Redeem</th>
+                    </tr>
+                  </thead>
+                  <tr>
+                    <td>1</td>
+                    <td>Customer Name<br />Customer Email<br />Customer Phone No</td>
+                    <td>Filmhouse, Lekki<br />Avengers Endgame<br />24-Mar-2019; 11:40Am</td>
+                    <td>Date: 15-Mar-2019<br/>Time: 6:05 PM<br/>Category: Adult</td>
+                    <td>ISSUED<br /></td>
+                    <td>Qty: 2<br/>Amt: N4,000</td>
+                    <td><Button>Redeem</Button></td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Customer Name<br />Customer Email<br />Customer Phone No</td>
+                    <td>Filmhouse, Lekki<br />Avengers Endgame<br />24-Mar-2019; 11:40Am</td>
+                    <td>Date: 15-Mar-2019<br />Time: 6:05 PM<br />Category: Adult</td>
+                    <td>ISSUED<br /></td>
+                    <td>Qty: 2<br />Amt: N4,000</td>
+                    <td><Button>Redeem</Button></td>
+                  </tr>
+                </Table>
               </CardBody>
             </Card>
           </Col>
