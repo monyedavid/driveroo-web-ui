7afafffff9eda6ee70f4da3bdfe38471168d2e86
rasheeddavid <griffinc317@gmail.com>
2019-05-08 11:22:34 +0100

    weekly transaction modal

--- src/pages/transaction/WeeklyTransactions.jsx
+++ src/pages/transaction/WeeklyTransactions.jsx
@@ -2,16 +2,43 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Page from 'components/Page';
 import TransactionItem from '../../components/common/List/TransactionItem';
-import { Link, withRouter } from 'react-router-dom';
-import { Card, CardBody, CardHeader, Col, Table, Row } from 'reactstrap';
-import { getWeeklyTransaction } from '../../redux/actions/transaction';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Table,
+  Row,
+} from 'reactstrap';
+import {
+  getWeeklyTransaction,
+  setTransaction,
+} from '../../redux/actions/transaction';
+
+import { withRouter } from 'react-router-dom';
+// MODAL CREATE DYNAMIC MODALS
+import Dialogue from '../../components/common/Modal/a.index';
+import DialogueTitle from '../../components/common/Modal/Title';
+import DialogueContent from '../../components/common/Modal/Content';
+import DialogueAction from '../../components/common/Modal/Action';
+// SPINNER IMAGES
+import LoadSpinner from '../../components/common/spinner';
+import spin from '../../utils/spin.gif';
+
+// TRANSACTION DETAILS
+import { TransactionDetail } from './TransactionDetails';
 
 class WeeklyTransactions extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      weekback: 3,
+      modal: false,
+      weekback: 7,
     };
+
+    this.close = this.close.bind(this);
+    this.toggle = this.toggle.bind(this);
   }
 
   componentDidMount() {
@@ -21,11 +48,89 @@ class WeeklyTransactions extends Component {
     // get weekly transactions for last one week
   }
 
+  toggle(index) {
+    const { transaction, setTransaction } = this.props;
+    let data = transaction.weekly;
+
+    if (typeof index === 'number') {
+      console.log('YAHH');
+      setTransaction(data[index]);
+    }
+    this.setState(prevState => ({
+      modal: !prevState.modal,
+    }));
+  }
+
+  close() {
+    this.setState(prevState => ({
+      modal: !prevState.modal,
+    }));
+  }
+
   render() {
     const {
-      transaction: { loading, weekly },
+      transaction: { loading, weekly, detail },
     } = this.props;
     let transactionItem;
+
+    const externalCloseBtn = (
+      <button
+        className="close"
+        style={{ position: 'absolute', top: '15px', right: '15px' }}
+        onClick={this.close}
+      >
+        &times;
+      </button>
+    );
+
+    let dialogueTitle;
+    let dialogueContent;
+    let transactionDialog;
+    let dialogueAction;
+
+    if (weekly && !loading) {
+      // populates data table loaidng a merchant??
+      dialogueTitle = (
+        <DialogueTitle
+          simpleContext={
+            detail === null || detail === undefined ? '' : detail.message
+          }
+        />
+      );
+      dialogueContent = (
+        <DialogueContent
+          contextComponent={
+            detail === null ? (
+              <LoadSpinner src={spin} />
+            ) : (
+              <TransactionDetail {...detail} />
+            )
+          }
+        />
+      );
+      dialogueAction = (
+        <DialogueAction
+          contextComponent={
+            <Button color="secondary" onClick={this.close}>
+              Cancel
+            </Button>
+          }
+        />
+      );
+      transactionDialog = (
+        <Dialogue
+          DialogTitle={dialogueTitle}
+          DialogContent={dialogueContent}
+          // DialogActions={dialogueAction}
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          // className={}
+          externalCloseBtn={externalCloseBtn}
+        />
+      );
+    } else {
+      transactionDialog = <React.Fragment />;
+    }
 
     /**
      *  @component   TransactionItem
@@ -43,6 +148,7 @@ class WeeklyTransactions extends Component {
       ? (transactionItem = weekly.map((details, i) => (
           <TransactionItem
             key={details._id}
+            toggle={this.toggle}
             index={i}
             type={2}
             transactionDetails={details}
@@ -51,43 +157,46 @@ class WeeklyTransactions extends Component {
       : (transactionItem = <h4>No Movie Tickets Where Found!</h4>);
 
     return (
-      <Page
-        title="Weekly Transactions"
-        breadcrumbs={[{ name: 'Weekly Transactions', active: true }]}
-      >
-        <Row>
-          <Col xl={12} lg={12} md={12}>
-            <Card>
-              <CardHeader>
-                Weekly Transactions from {this.state.weekback}{' '}
-                {this.state.weekback > 1 ? 'weeks' : 'week'} ago
-              </CardHeader>
-              <CardBody>
-                <Col>
-                  <Card body>
-                    <Table striped={true} id="weeklytranstable">
-                      <thead>
-                        <tr>
-                          <th>Trans ID</th>
-                          <th>UID</th>
-                          <th>Amount</th>
-                          <th>Platform.</th>
-                          <th>Event Name</th>
-                          <th>Customer Name</th>
-                          <th>Date</th>
-                          <th>Details</th>
-                        </tr>
-                      </thead>
+      <React.Fragment>
+        {transactionDialog}
+        <Page
+          title="Weekly Transactions"
+          breadcrumbs={[{ name: 'Weekly Transactions', active: true }]}
+        >
+          <Row>
+            <Col xl={12} lg={12} md={12}>
+              <Card>
+                <CardHeader>
+                  Weekly Transactions from {this.state.weekback}{' '}
+                  {this.state.weekback > 1 ? 'weeks' : 'week'} ago
+                </CardHeader>
+                <CardBody>
+                  <Col>
+                    <Card body>
+                      <Table striped={true} id="weeklytranstable">
+                        <thead>
+                          <tr>
+                            <th>Trans ID</th>
+                            <th>UID</th>
+                            <th>Amount</th>
+                            <th>Platform.</th>
+                            <th>Event Name</th>
+                            <th>Customer Name</th>
+                            <th>Date</th>
+                            <th>Details</th>
+                          </tr>
+                        </thead>
 
-                      {transactionItem}
-                    </Table>
-                  </Card>
-                </Col>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </Page>
+                        {transactionItem}
+                      </Table>
+                    </Card>
+                  </Col>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Page>
+      </React.Fragment>
     );
   }
 }
@@ -98,5 +207,5 @@ const map_state_to_props = state => ({
 
 export default connect(
   map_state_to_props,
-  { getWeeklyTransaction },
+  { getWeeklyTransaction, setTransaction },
 )(withRouter(WeeklyTransactions));
--- src/redux/axios.request.link.js
+++ src/redux/axios.request.link.js
@@ -1,3 +1,3 @@
-export const MOVIE_DASHBOARD_API_LOCAL =
+export const MOVIE_DASHBOARD_API =
     'https://expressjs-starter-kit-yjwapsqcyk.now.sh/api/v1',
-  MOVIE_DASHBOARD_API = 'http://localhost:5000/api/v1';
+  MOVIE_DASHBOARD_API_LOCAL = 'http://localhost:5000/api/v1';
