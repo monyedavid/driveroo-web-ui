6c52d2d4a67114dc2885f4a4dd19c92fb23c226d
Joshua Oluikpe <joshuaoluikpe@gmail.com>
2019-08-14 14:24:46 +0100

    Driver | Solutions

--- src/redux/actions/drivers/index.js
+++ src/redux/actions/drivers/index.js
@@ -1,31 +1,30 @@
 import { GSDriver } from '../../../Graphql/driver.graphql';
-import { GET_ERRORS } from '../../types';
+import { GET_ERRORS, GET_DRIVERS } from '../../types';
 
 export const allDrivers = token => async dispatch => {
   const service = new GSDriver(token);
   let result;
   try {
     result = await service.getDriversDetails();
+    if (result)
+      if (result.data) {
+        if (result.data.findAllDrivers[0].__typename === 'Error') {
+          dispatch({
+            type: GET_ERRORS,
+            payload: result.data.findAllDrivers,
+          });
+        }
+
+        if (result.data.findAllDrivers[0].__typename === 'DriverResults')
+          dispatch({
+            type: GET_DRIVERS,
+            payload: result.data.findAllDrivers,
+          });
+      }
   } catch (error) {
     dispatch({
       type: 'NETWORK',
       payload: 'A network error occured please try again later',
     });
-
-    // if (result)
-    //   if (result.data) {
-    //     if (result.data.login[0].path) {
-    //       dispatch({
-    //         type: GET_ERRORS,
-    //         payload: result.data.login,
-    //       });
-    //     }
-
-    //     if (result.data.login)
-    //       if (!result.data.login[0].path) {
-    //       }
-    //   }
   }
-
-  console.log(result);
 };
